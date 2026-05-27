f87187d6cd194b8e39a3dad06867772493b725bd
Fábio Adriano Corrêa de Oliveira Júnior <fjrcorreoli@gmail.com>
2026-05-26 22:23:36 -0300

    Acrescentado Firebase

---
+++ app/sistema/paginas/curriculos/types.ts
@@ -0,0 +1,26 @@
+import type { Timestamp } from "firebase/firestore";
+
+export type Experiencia = {
+  cargo: string;
+  empresa: string;
+  periodo: string;
+  descricao: string;
+};
+
+export type CurriculoFormData = {
+  nomeCompleto: string;
+  email: string;
+  telefone: string;
+  cpf: string;
+  profissao: string;
+  resumoProfissional: string;
+  formacao: string[];
+  experiencia: Experiencia[];
+  habilidades: string[];
+  imagem: string;
+};
+
+export type Curriculo = CurriculoFormData & {
+  id: string;
+  criadoEm?: Timestamp | Date | null;
+};
